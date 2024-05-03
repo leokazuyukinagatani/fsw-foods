b22687b06b4eb0dc616f3245b3cf5fdb1fea1c4a
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-05-03 07:07:41 -0300

    feat: add category item and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import CategoryList from "./_components/category-list";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <div className="px-5">
+        <Header />
+      </div>
       <div className="px-5 pt-6">
         <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   );
