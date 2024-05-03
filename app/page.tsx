35251a03b0d2678cf8a354983625cca9da9e244f
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-05-03 07:18:32 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,17 @@ export default function Home() {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Ate 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 }
