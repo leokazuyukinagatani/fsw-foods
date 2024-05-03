8adfe3cc79b48d89a100f235e23e65209943d942
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-05-03 06:35:50 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
